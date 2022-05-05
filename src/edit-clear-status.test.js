96044ddc0ae8e3378a9aa04bf6dcdac0b57f6300
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-05-05 22:24:22 +0200

    Test status, clear all and edit in UI

--- src/edit-clear-status.test.js
+++ src/edit-clear-status.test.js
@@ -1,4 +1,3 @@
-
 import {
   checkStatus,
   getTasks,
@@ -8,6 +7,8 @@ import {
 } from './methods.js';
 import {
   uiCheckStatus,
+  uiBlurTextArea,
+  uiClearCompleted,
 } from './ui.js';
 
 const html = `<section id="todo-list">
@@ -24,16 +25,15 @@ const html = `<section id="todo-list">
 </section>`;
 document.body.innerHTML = html;
 
-const liElement = `<li>
-  <input id="ck-id" class="check-box" type="checkbox" name="checkBox">
+const liElement = `<li class="list-items" data-taskid="1">
+  <input id="ck-id" class="check-box" type="checkbox" name="checkBox" checked>
   <label class="lb-task" id="lb-id" for="ta-id">description</label>
-  <textarea id="ta-id" class="ta-task" name="edit-task">description</textarea>
+  <textarea id="ta-id" class="ta-task" name="edit-task">new description</textarea>
   <span class="dots material-symbols-outlined">more_vert</span>
   <span class="trash material-symbols-outlined">delete</span>
 </li>`;
 
-/* check Status, Clear All*/
-
+// check Status, Clear All
 describe('Edit, Clear All, Check Status in local Storage', () => {
   test('Check Status in local Storage', () => {
     setTasks([{
@@ -49,23 +49,23 @@ describe('Edit, Clear All, Check Status in local Storage', () => {
 
   test('Clear All in local Storage', () => {
     setTasks([{
-        description: 'wash dishes',
-        isCompleted: true,
-        index: 1,
-        id: 1,
-      },
-      {
-        description: 'wash dishes',
-        isCompleted: false,
-        index: 2,
-        id: 2,
-      },
-      {
-        description: 'wash dishes',
-        isCompleted: true,
-        index: 3,
-        id: 3,
-      }
+      description: 'wash dishes',
+      isCompleted: true,
+      index: 1,
+      id: 1,
+    },
+    {
+      description: 'wash dishes',
+      isCompleted: false,
+      index: 2,
+      id: 2,
+    },
+    {
+      description: 'wash dishes',
+      isCompleted: true,
+      index: 3,
+      id: 3,
+    },
     ]);
 
     clearAllBtn();
@@ -76,18 +76,71 @@ describe('Edit, Clear All, Check Status in local Storage', () => {
     expect(items[0].index).toEqual(1);
   });
 
-  test ('Edit in local Storage', () => {
+  test('Edit in local Storage', () => {
     setTasks([{
       description: 'wash the car',
       isCompleted: false,
       index: 1,
       id: 1,
     }]);
-    const newValue = 'go out for dinner'
+    const newValue = 'go out for dinner';
     updateTask('1', newValue);
     const items = getTasks();
     expect(items).toBeDefined();
     expect(items).toHaveLength(1);
     expect(items[0].description).toBe(newValue);
-  })
+  });
+});
+
+describe('Status, Clear all, Edit in UI', () => {
+  test('uiCheckStatus function in UI', () => {
+    setTasks([{
+      description: 'wash the car',
+      isCompleted: false,
+      index: 1,
+      id: 1,
+    }]);
+    const ulElement = document.querySelector('.check-list');
+    ulElement.innerHTML = liElement;
+    const checkBox = document.querySelector('.check-box');
+    const event = {
+      target: checkBox,
+    };
+    uiCheckStatus(event);
+    const items = checkBox.parentNode;
+    expect(items).toBeDefined();
+    expect(items.classList.contains('completed')).toBe(true);
+  });
+
+  test('uiClearAllBtn function in UI', () => {
+    const ulElement = document.querySelector('.check-list');
+    ulElement.innerHTML = liElement;
+    uiClearCompleted();
+    const items = liElement;
+    expect(items).toBeDefined();
+    expect(ulElement.hasChildNodes()).toBe(false);
+  });
+
+  test('uiBlurTextArea (edit) function in UI', () => {
+    setTasks([{
+      description: 'wash the car',
+      isCompleted: false,
+      index: 1,
+      id: 1,
+    }]);
+    const ulElement = document.querySelector('.check-list');
+    ulElement.innerHTML = liElement;
+    const taElement = document.querySelector('.ta-task');
+    const event = {
+      target: taElement,
+    };
+    const newValue = taElement.value;
+    uiBlurTextArea(event);
+    const lblElement = document.querySelector('.lb-task');
+    expect(lblElement.textContent).toBe(newValue);
+    const items = getTasks();
+    expect(items).toBeDefined();
+    expect(items).toHaveLength(1);
+    expect(items[0].description).toBe(newValue);
+  });
 });
